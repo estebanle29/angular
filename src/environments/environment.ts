48c7f7eabb555d1ad3a33fc30a386d963591c5fa
AndyNoir998 <amorenoh1@ucentral.edu.co>
2023-04-11 19:42:32 -0500

    1000Agro proyecto web

---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: false,
+  
+  firebase: {
+    apiKey: "AIzaSyD5jPjkBMklsHQ55wgUIr7tE8f8oXoiQ9A",
+    authDomain: "prueba-8aac2.firebaseapp.com",
+    databaseURL: "https://prueba-8aac2-default-rtdb.firebaseio.com",
+    projectId: "prueba-8aac2",
+    storageBucket: "prueba-8aac2.appspot.com",
+    messagingSenderId: "832416428481",
+    appId: "1:832416428481:web:53e83f1ee9186ba05cff40",
+    measurementId: "G-HP43EKDK56"
+  },
+  UNSPLASH_SOURCE_API:
+    'https://source.unsplash.com/1280x900/?restaurants,food,alcohol',
+    
+};
